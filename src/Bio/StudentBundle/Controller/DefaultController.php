def6a7b161ce6ff067779c0540980858fd08fff0
Nicholas <nickclaw@gmail.com>
2013-06-28 08:41:46 -0700

    Seperate functions for add/delete/edit/upload.
    Removed the code responsible for actually adding, deleting, editing,
    and uploading from the routes for reusability. Created a BioException
    class to handle sending messages back for display.

--- src/Bio/StudentBundle/Controller/DefaultController.php
+++ src/Bio/StudentBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 use Bio\StudentBundle\Entity\Student;
+use Bio\StudentBundle\Exception\BioException;
 
 /**
  * @Route("/student")
@@ -35,14 +36,12 @@ class DefaultController extends Controller
     	if ($request->getMethod() === "POST") {
     		$form->handleRequest($request);
     		if ($form->isValid()) {
-    			$em = $this->getDoctrine()->getManager();
     			try {
-	    			$em->persist($entity);
-	    			$em->flush();
-	    			$request->getSession()->getFlashBag()->set('success', 'Student added :)');
-	    		} catch (\Doctrine\DBAL\DBALException $e) {
-	    			$request->getSession()->getFlashBag()->set('failure', 'That ID or Email is already in the database :(');
-	    		}
+                    $this->addStudent($entity);
+                    $request->getSession()->getFlashBag()->set('success', 'Student added!');
+                } catch (BioException $e) {
+                    $request->getSession()->getFlashBag()->set('failure', $e->getMessage());
+                }
 	    		$form = $cloned;
     		} else {
     			$request->getSession()->getFlashBag()->set('failure', 'There was an error. Please try again :(');
@@ -66,17 +65,14 @@ class DefaultController extends Controller
     	if ($request->getMethod() === "POST") {
     		$form->handleRequest($request);
     		$sid = $form->get('sid')->getData();
-    		$em = $this->getDoctrine()->getManager();
-    		$repo = $em->getRepository('BioStudentBundle:Student');
 
-    		$entity = $repo->findOneBySid($sid);
-    		if (!$entity) {
-    			$request->getSession()->getFlashBag()->set('failure', "Student #".$sid." does not exist");
-    		} else {
-    			$em->remove($entity);
-    			$em->flush();
-    			$request->getSession()->getFlashBag()->set('success', "Student #".$sid." removed.");
-    		}
+            try {
+                $this->deleteStudent($sid);
+                $request->getSession()->getFlashBag()->set('success', "Student #".$sid." removed.");
+            } catch (BioException $e) {
+                $request->getSession()->getFlashBag()->set('failure', $e->getMessage());
+            }
+    		
     	}
 
     	return array('form' => $cloned->createView());
@@ -101,22 +97,95 @@ class DefaultController extends Controller
     	if ($request->getMethod() === "POST") {		// if request was sent
     		$form->handleRequest($request);
     		if ($form->isValid()) {					// and form was valid
-    			$em = $this->getDoctrine()->getManager();
-    			$repo = $em->getRepository('BioStudentBundle:Student');
-    			$dbEntity = $repo->findOneBySid($entity->getSid());		// try to find the student in database
-    			if (!$dbEntity) {										// if student does not exist
-    				$request->getSession()->getFlashBag()->set('failure', 'Student #'.$entity->getSid()." does not exist.");
-    			} else {					// student does exist
-    				$dbEntity->setFName($entity->getFName());
-    				$dbEntity->setLName($entity->getLName());
-    				$dbEntity->setEmail($entity->getEmail());
-    				// for more changes
-    				$em->flush();
-    				$request->getSession()->getFlashBag()->set('success', "Student #".$entity->getSid()." updated.");
-    			}
+    			try {
+                    $this->editStudent($entity);
+                    $request->getSession()->getFlashBag()->set('success', "Student #".$entity->getSid()." updated.");
+                } catch (BioException $e) {
+                    $request->getSession()->getFlashBag()->set('failure', $e->getMessage());
+                }
     		}
     	}
 
     	return array('form' => $form->createView());
+    }
+	/**
+     * @Route("/upload")
+     * @Template()
+     */
+    public function uploadAction(Request $request) {
+    	$form = $this->createFormBuilder()
+    		->add('file', 'file')
+    		->add('Upload', 'submit')
+    		->getForm();
+
+    	if ($request->getMethod() === "POST") {
+    		$form->handleRequest($request);
+    		$data = $form->get('file')->getData();
+    		if ($data !== null) {
+    			$file = file($data);
+    			$em = $this->getDoctrine()->getManager();
+	    		for ($i = 1; $i < count($file); $i++) {
+	    			$array = explode(",", $file[$i]);
+	    			try {
+	    				$entity = new Student();
+	    				$entity->setSid(intval($array[0]));
+	    				$entity->setEmail($array[7]);
+	    				$entity->setFName($array[2]);
+	    				$entity->setLName($array[1]);
+	    				$em->persist($entity);
+	    			} catch(\Doctrine\DBAL\DBALException $e) {
+
+	    			}
+	    		}
+			$em->flush();
+	    	}
+    	}
+    	return array("form" => $form->createView());
+    }
+
+    private function addStudent($entity) {
+        $em = $this->getDoctrine()->getManager();
+        try {
+            $em->persist($entity);
+            $em->flush();
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new BioException("That Student ID or email is already registered.");
+        }
+    }
+
+    private function deleteStudent($sid) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('BioStudentBundle:Student');
+
+        $entity = $repo->findOneBySid($sid);
+        if (!$entity) {
+            throw new BioException('We could not find a student with that ID.');
+        } else {
+            $em->remove($entity);
+            $em->flush();
+        }
+    }
+
+    private function editStudent($entity) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('BioStudentBundle:Student');
+        $dbEntity = $repo->findOneBySid($entity->getSid());     // try to find the student in database
+        if (!$dbEntity) {                                       // if student does not exist
+            throw new BioException("We could not find a student with that ID.");
+        } else {                    // student does exist
+            try {
+                $dbEntity->setFName($entity->getFName());
+                $dbEntity->setLName($entity->getLName());
+                $dbEntity->setEmail($entity->getEmail());
+                // for more changes
+                $em->flush();
+            } catch (\Doctrine\DBAL\DBALException $e) {
+                throw new BioException('A student already has that email.');
+            }
+        }
+    }
+
+    private function uploadStudentList($file) {
+        
     }
 }
