ef00f3a8d73598626e72ceb13417fede34e2e596
Nicholas <nickclaw@gmail.com>
2013-07-01 14:27:46 -0700

    Sidebar Action
    Sidebar is now rendered via the BioStyleBundle controller so it can
    update based on the current route.

--- src/Bio/StyleBundle/Controller/DefaultController.php
+++ src/Bio/StyleBundle/Controller/DefaultController.php
@@ -8,5 +8,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+	/**
+	 * @Template()
+	 * 
+	 */
+	public function sidebarAction($route) {
+		$options = array();
+		$options['students'] = array();
+			$options['students']['display'] = 'display_students';
+			$options['students']['find'] = 'find_student';
+			$options['students']['add'] = 'add_student';
+			$options['students']['upload'] = 'upload_student';
 
+		$options['clickers'] = array();
+			$options['clickers']['register'] = 'display_students';
+			$options['clickers']['unregister'] = 'display_students';
+
+		if ($route == 'display_students' || $route == 'find_student' || 
+			$route == 'edit_student' || $route == 'add_student' || 
+			$route == 'upload_student') {
+				$top = 'students';
+		}
+
+		return array('top' => $top, 'options' => $options);
+	}
 }
