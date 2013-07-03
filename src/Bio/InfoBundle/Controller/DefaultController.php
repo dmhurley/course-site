c964e3078e3e95b902b85dc202cd320a97228b8d
Nicholas <nickclaw@gmail.com>
2013-07-03 11:14:16 -0700

    Link Entity, Announcement Seperation
    Created an entity to represent a link. Seperated announcement logic
    into it's own controller.

--- src/Bio/InfoBundle/Controller/DefaultController.php
+++ src/Bio/InfoBundle/Controller/DefaultController.php
@@ -19,7 +19,7 @@ use Bio\DataBundle\Exception\BioException;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/edit/course", name="edit_info")
+     * @Route("/edit", name="edit_info")
      * @Template()
      */
     public function indexAction(Request $request) {
@@ -83,106 +83,5 @@ class DefaultController extends Controller
     	}
 
         return array('form' => $form->createView(), 'title' => "Edit Course Information");
-    }
-
-    /**
-     * @Route("/announcements", name="view_announcements")
-     * @Template()
-     */
-    public function announcementsAction(Request $request) {
-        $ann = new Announcement();
-        $ann->setTimestamp(new \DateTime());
-        $ann->setExpiration((new \DateTime())->modify('+1 day'));
-        $form = $this->createFormBuilder($ann)
-            ->add('timestamp', 'datetime', array('attr' => array('class' => 'datetime')))
-            ->add('expiration', 'datetime', array('attr' => array('class' => 'datetime')))
-            ->add('text', 'textarea')
-            ->add('add', 'submit')
-            ->getForm();
-
-
-        $db = new Database($this, 'BioInfoBundle:Announcement');
-        if ($request->getMethod() === "POST") {
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $db->add($ann);
-                $db->close();
-                $request->getSession()->getFlashBag()->set('success', 'Announcement added.');
-            } else {
-                $request->getSession()->getFlashBag()->set('failure', 'Whoops.');
-            }
-        }
-
-        $anns = $db->find(array(), array('expiration' => 'DESC'));
-        return array('form' => $form->createView(),'anns' => $anns, 'title' => 'Edit Announcements');
-    }
-
-    /**
-     * @Route("/delete/announcement", name="delete_announcement")
-     */
-    public function deleteAction(Request $request) {
-        if ($request->getMethod() === "GET" && $request->query->get('id')) {
-            $id = $request->query->get('id');
-
-            $db = new Database($this, 'BioInfoBundle:Announcement');
-
-            $ann = $db->findOne(array('id'=>$id));
-            if ($ann) {
-                $db->delete($ann);
-                $db->close();
-                $request->getSession()->getFlashBag()->set('success', 'Announcement deleted.');
-            } else {
-                $request->getSession()->getFlashBag()->set('failure', 'Could not find that announcement.');
-            }
-        }
-
-        if ($request->headers->get('referer')){
-            return $this->redirect($request->headers->get('referer'));
-        } else {
-            return $this->redirect($this->generateUrl('view_announcements'));
-        }
-    }
-
-    /**
-     * @Route("/edit/announcement", name="edit_announcement")
-     * @Template()
-     */
-    public function editAction(Request $request) {
-        $ann = new Announcement(); // her??
-        $db = new Database($this, 'BioInfoBundle:Announcement');
-
-        if ($request->getMethod() === "GET" && $request->query->get('id')){
-            $id = $request->query->get('id');
-
-            $ann = $db->findOne(array('id' => $id));
-        } else {
-            $ann = new Announcement();
-        }
-
-        $form = $this->createFormBuilder($ann)
-            ->add('timestamp', 'datetime', array('attr' => array('class' => 'datetime')))
-            ->add('expiration', 'datetime', array('attr' => array('class' => 'datetime')))
-            ->add('text', 'textarea')
-            ->add('id', 'hidden')
-            ->add('edit', 'submit')
-            ->getForm();
-
-        if ($request->getMethod() === "POST") {
-            $form->handleRequest($request);
-
-            if($form->isValid()) {
-                $dbAnn = $db->findOne(array('id' => $ann->getId()));
-                $dbAnn->setTimestamp($ann->getTimestamp())
-                    ->setExpiration($ann->getExpiration())
-                    ->setText($ann->getText());
-
-                $db->close();
-
-                return $this->redirect($this->generateUrl('view_announcements'));
-            }
-        }
-
-        return array('form' => $form->createView(), 'title' => 'Edit Announcement');
     }
 }
---
+++ src/Bio/InfoBundle/Entity/Link.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Bio\InfoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Link
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Link
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=2048)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255)
+     */
+    private $location;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Link
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Link
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     * @return Link
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string 
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+}
