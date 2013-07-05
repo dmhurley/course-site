31b51ebe4fb4d1d2c7074257e23a688ac0a4d921
Nicholas <nickclaw@gmail.com>
2013-07-05 08:26:50 -0700

    YAML sidebar
    Sidebar links are now defined using a YAML file. Allows the easy
    addition of parameters.

--- src/Bio/StyleBundle/Controller/DefaultController.php
+++ src/Bio/StyleBundle/Controller/DefaultController.php
@@ -6,44 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Dumper;
+
 class DefaultController extends Controller
 {
 	/**
 	 * @Template()
 	 */
 	public function sidebarAction($route) {
-		$options = array();
-		$options['Course Info'] = array();
-			$options['Course Info']['Course Info'] = 'edit_info';
-			$options['Course Info']['Edit Info'] = 'edit_info';
-			$options['Course Info']['Announcements'] = 'view.announcement';
-			$options['Course Info']['Links'] = 'view.link';
-			$options['Course Info']['Instructors'] = 'view.person';
-
-		$options['students'] = array();
-			$options['students']['students'] = 'display_students';
-			$options['students']['display'] = 'display_students';
-			$options['students']['find'] = 'find_student';
-			$options['students']['add'] = 'add_student';
-			$options['students']['upload'] = 'upload_student';
-
-		$options['clickers'] = array();
-			$options['clickers']['clickers'] = 'register_clicker';
-			$options['clickers']['register'] = 'register_clicker';
-			$options['clickers']['download'] = 'download_list';
-			$options['clickers']['clear'] = 'clear_list';
+		$yaml = new Parser();
+		$options = $yaml->parse(file_get_contents('bundles/biostyle/sidebar.yml'));
 
 		if ($route == 'display_students' || $route == 'find_student' || 
 			$route == 'edit_student' || $route == 'add_student' || 
 			$route == 'upload_student') {
-				$top = 'students';
+				$expanded = 'Students';
 		} else if ( $route == 'register_clicker' || $route == 'download_list' ||
 					$route == 'clear_list') {
-				$top = 'clickers';
+				$expanded = 'Clickers';
 		} else if ( $route == 'edit_info' || $route == "view" || $route == "delete" || $route == 'edit') {
-			$top = 'Course Info';
+			$expanded = 'Course Info';
 		}
 
-		return array('top' => $top, 'options' => $options);
+		// $yaml = new Dumper();
+		// echo $yaml->dump($options1);
+
+
+		return array('expanded' => $expanded, 'options' => $options);
 	}
 }
