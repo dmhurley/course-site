8a2f87168c14e0c276261599d0d5d948a635e60b
Nicholas <nickclaw@gmail.com>
2013-07-25 15:04:03 -0700

    Trip Entity Setup Created a trip entity and ability to create/display them

--- src/Bio/TripBundle/Controller/DefaultController.php
+++ src/Bio/TripBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Bio\DataBundle\Objects\Database;
 use Bio\DataBundle\Exception\BioException;
+use Bio\TripBundle\Entity\Trip;
 
 /** 
  * @Route("/admin/trip")
@@ -21,7 +22,30 @@ class DefaultController extends Controller
      * @Template()
      */
     public function indexAction(Request $request)
-    {
-        return array( 'title' => "Manage Trips");
+    {	
+    	$trip = new Trip();
+    	$form = $this->createFormBuilder($trip)
+    		->add('title', 'text', array('label' => 'Title:'))
+    		->add('shortSum', 'textarea', array('label' => 'Short Summary:'))
+    		->add('longSum', 'textarea', array('label' => 'Long Summary:'))
+    		->add('start', 'datetime', array('label' => 'Start:', 'attr' => array('class' => 'datetime')))
+    		->add('end', 'datetime', array('label' => 'End:', 'attr' => array('class' => 'datetime')))
+    		->add('max', 'integer', array('label' => 'Limit:'))
+    		->add('email', 'email', array('label' => 'Leader Email:'))
+    		->add('add', 'submit')
+    		->getForm();
+
+    	$db = new Database($this, 'BioTripBundle:Trip');
+
+    	if ($request->getMethod() === "POST") {
+    		$form->handleRequest($request);
+    		if ($form->isValid()) {
+
+    			$db->add($trip);
+    			$db->close();
+    		}
+    	}
+    	$trips = $db->find(array(), array(), false);
+        return array('form' => $form->createView(), 'trips' => $trips, 'title' => "Manage Trips");
     }
 }
