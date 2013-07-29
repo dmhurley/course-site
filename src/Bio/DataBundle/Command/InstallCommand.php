f1a762a32c487fccec44069f74bac8106e533ec0
Nicholas <nickclaw@gmail.com>
2013-07-29 14:59:05 -0700

    Routing Installer
    Installs routing for optional bundles too, also rewrote the sidebar
    installation function.

--- src/Bio/DataBundle/Command/InstallCommand.php
+++ src/Bio/DataBundle/Command/InstallCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Symfony\Component\Security\Core\User\UserInterface;
-
 use Symfony\Component\Yaml\Yaml;
 
 class InstallCommand extends ContainerAwareCommand
@@ -38,39 +37,73 @@ class InstallCommand extends ContainerAwareCommand
 
         if ($input->getOption('all')) {
             $output->writeln('Installing all bundles');
-            $this->install(array('exam'), $output);
+            $this->setSidebar(array('exam', 'trip'), $output);
+            $this->setRouting(array('exam', 'trip'), $output);
         } else {
             if (count($bundles) === 0) {
                 $output->writeln("OHNOES");
             } else {
-                $this->install($bundles, $output);
+                $this->setSidebar($bundles, $output);
+                $this->setRouting($bundles, $output);
             }
             $output->writeln(implode($bundles, " "));
         }
     }
 
-    private function install(array $bundles, $output) {
-        $src = 'app/config/parameters.yml.dist';
+    private function setSidebar(array $bundles, OutputInterface $output) {
+        $distribution = 'app/config/parameters.yml.dist';
         $destination = 'app/config/parameters.yml';
-        if (file_exists($destination) && file_exists($src)){
 
-            $parameters = Yaml::parse($src);
+        if (!file_exists($distribution) || !file_exists($destination)) {
+            $output->writeln("Couldn't find parameters.yml[.dist] file.");
+        } else {
+            $dist = Yaml::parse($distribution);
+            $dest = Yaml::parse($destination);
 
-            foreach($bundles as $bundleName){
-                $file = 'src/Bio/'.ucFirst($bundleName).'Bundle/Resources/config/sidebar.yml';
+            $dest['parameters']['sidebar'] = $dist['parameters']['sidebar'];
 
-                if (file_exists($file)) {
-                    $bundle = Yaml::parse($file);
-                    $bundleKeys = array_keys($bundle);
-                    $parameters['parameters']['sidebar'][$bundleKeys[0]] = $bundle[$bundleKeys[0]];
+            foreach($bundles as $bundleName) {
+                $configFileName = 'src/Bio/'.ucFirst($bundleName).'Bundle/Resources/config/sidebar.yml';
+                if (file_exists($configFileName)){
+                    $src = Yaml::parse($configFileName);
+                    $srcKeys = array_keys($src);
+
+                    $dest['parameters']['sidebar'][$srcKeys[0]] = $src[$srcKeys[0]];
+
                 } else {
-                    $output->writeln(getcwd().'/'.$file." does not exist.");
+                    $output->writeln('Could not find file: '.getcwd().'/'.$configFileName);
                 }
             }
 
-            file_put_contents($destination, Yaml::dump($parameters, 6, 4));
-        } else {
-            $output->writeln("NO PARAMETERS FILE");
+            file_put_contents($destination, Yaml::dump($dest, 6, 4));
         }
     }
+
+    private function setRouting(array $bundles, OutputInterface $output) {
+        $distribution = 'app/config/routing.yml.dist';
+        $destination = 'app/config/routing.yml';
+
+        if (!file_exists($distribution) || !file_exists($destination)) {
+            $output->writeln("Couldn't find parameters.yml[.dist] file.");
+        } else {
+            $dist = Yaml::parse($distribution);
+            $dest = Yaml::parse($destination);
+
+            foreach ($bundles as $bundleName) {
+                $configFileName = 'src/Bio/'.ucFirst($bundleName).'Bundle/Resources/config/routing.yml';
+
+                if (file_exists($configFileName)) {
+                    $src = Yaml::parse($configFileName);
+                    $srcKeys = array_keys($src);
+
+                    $dest[$srcKeys[0]]= $src[$srcKeys[0]];
+                } else {
+                    $output->writeln('Could not find file: '.getcwd().'/'.$configFileName);
+                }
+            }
+
+            file_put_contents($destination, Yaml::dump($dest, 6, 4));
+        }
+    }
+        
 }
