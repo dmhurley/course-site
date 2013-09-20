421ffc6116e49b6088709655180743f83cb73898
Nicholas <nickclaw@gmail.com>
2013-09-20 13:03:13 -0700

    Last of the migrations

--- app/DoctrineMigrations/Version20130913111233.php
+++ app/DoctrineMigrations/Version20130913111233.php
@@ -5,17 +5,11 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration,
     Doctrine\DBAL\Schema\Schema;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface,
-    Symfony\Component\DependencyInjection\ContainerInterface;
-
-use Bio\InfoBundle\Entity\CourseSection;
-
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20130913111233 extends AbstractMigration implements ContainerAwareInterface
+class Version20130913111233 extends AbstractMigration
 {
-    private $container;
 
     public function up(Schema $schema)
     {
@@ -33,31 +27,37 @@ class Version20130913111233 extends AbstractMigration implements ContainerAwareI
         $this->addSql("ALTER TABLE Info ADD days LONGTEXT NOT NULL COMMENT '(DC2Type:array)', ADD startTime TIME NOT NULL, ADD endTime TIME NOT NULL, ADD bldg VARCHAR(255) NOT NULL, ADD room VARCHAR(255) NOT NULL");
     }
 
-    public function setContainer(ContainerInterface $container = null) {
-        $this->container = $container;
-    }
-
     public function postUp(Schema $schema) {
-        $em = $this->container->get('doctrine.orm.entity_manager');
-        $sections = $em->createQueryBuilder()->select('s')->from('BioInfoBundle:Section', 's')->getQuery()->getResult();
+        $conn = $this->connection;
+        $sections = $conn->fetchAll("SELECT name FROM Base WHERE type = 'section'");
 
         $cSections = [];
 
+        print_r($sections);
         foreach ($sections as $section) {
-            if (!in_array(substr($section->getName(), 0, 1), $cSections)) {
-                $cSection = new CourseSection();
-                $cSection->setName(substr($section->getName(), 0, 1))
-                    ->setDays(['m'])
-                    ->setStartTime(new \DateTime('midnight'))
-                    ->setEndTime(new \DateTime('midnight'))
-                    ->setBldg("HCK\tHitchcock Hall")
-                    ->setRoom("0");
-                $em->persist($cSection);
-                $cSections[] = $cSection->getName();
+            if (!in_array(substr($section['name'], 0, 1), $cSections)) {
+                $conn->insert('Base', array(
+                    'person_id' => NULL,
+                    'type' => 'course-section', 
+                    'timestamp' => NULL,
+                    'expiration' => NULL,
+                    'text' => NULL,
+                    'title' => NULL,
+                    'fName' => NULL,
+                    'lName' => NULL,
+                    'email' => NULL,
+                    'bldg' => 'HCK    Hitchcock Hall',
+                    'room' => '0',
+                    'name' => substr($section['name'], 0, 1),
+                    'start' => '00:00:00',
+                    'end' => '00:00:00',
+                    'days' => 'a:0:{}',
+                    'byAppointment' => NULL
+                    )
+                );
+
+                $cSections[] = substr($section['name'], 0, 1);
             }
         }
-        $em->flush();
     }
-
-
 }
