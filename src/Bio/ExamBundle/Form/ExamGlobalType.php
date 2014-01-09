88f8e8f673f7e4b61b729d102c81ca661977cb6f
Nicholas <nickclaw@gmail.com>
2014-01-09 12:37:11 -0800

    Exam form types

---
+++ src/Bio/ExamBundle/Form/ExamGlobalType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bio\ExamBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExamGlobalType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+           ->add('grade', 'integer', array('label' => "Tests To Grade:"))
+            ->add('comments', 'checkbox', array(
+                'label' => 'Force Comments',
+                'required' => false
+                )
+            )
+            ->add('rules', 'textarea', array('label' => "Test Rules:"))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bio\ExamBundle\Entity\ExamGlobal'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'form';
+    }
+}
---
+++ src/Bio/ExamBundle/Form/ExamType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Bio\ExamBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExamType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text', array('label'=>'Exam name'))
+            ->add('section', 'text', array(
+                'label'=>'Section name',
+                'required' => false,
+                'empty_data' => '',
+                'attr' => array(
+                    'pattern' => '(\A\Z)|(^[A-Z][A-Z0-9]?$)',
+                    'title' => 'One or two letter capitalized section name.'
+                    )
+                )
+            )
+            ->add('tDate', 'date',        array('label' => 'Test Date:'))
+            ->add('tStart', 'time',       array('label'=>'Test Start:'))
+            ->add('tEnd', 'time',         array('label'=>'Test End:'))
+            ->add('tDuration', 'integer', array('label'=>'Test length (m)'))
+            ->add('gDate', 'date',        array('label' => 'Grading Date:'))
+            ->add('gStart', 'time',       array('label'=>'Grade Start:'))
+            ->add('gEnd', 'time',         array('label'=>'Grade End:'))
+            ->add('gDuration', 'integer', array('label'=>'Grade length (m)'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bio\ExamBundle\Entity\Exam'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'form';
+    }
+}
