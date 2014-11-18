b65c57e6ea493e986ff8d5bf7ab42938a3f3486c
Nicholas Clawson <nickclaw@gmail.com>
2014-11-18 15:02:53 -0800

    Add public router for SurveyBundle

---
+++ src/Bio/SurveyBundle/Controller/PublicController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bio\SurveyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Bio\DataBundle\Objects\Database;
+use Bio\DataBundle\Exception\BioException;
+use Bio\SurveyBundle\Entity\Survey;
+use Bio\SurveyBundle\Entity\SurveyQuestion;
+
+
+/**
+ * @Route("/survey")
+ */
+class PublicController extends Controller
+{
+
+    /**
+     * @Route("/", name="view_surveys")
+     * @Template()
+     */
+    public function indexAction() {
+
+    }
+
+    /**
+     * @Route("/{id}", name="take_survey")
+     * @Template()
+     */
+    public function takeAction(Request $request, Survey $survey) {
+
+    }
+}
