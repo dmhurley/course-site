0010b2a875e3986f63f631a5b06c728ddfd7bb73
Nicholas Clawson <nickclaw@gmail.com>
2014-11-25 17:16:06 -0800

    Fixed case sensitivity migration bug.

---
+++ app/DoctrineMigrations/Version20141125145243.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141120145124 extends AbstractMigration
+{
+    private $hasTable;
+
+    public function preUp(Schema $schema) {
+        $this->hasTable = $schema->hasTable('filebase');
+    }
+
+    public function up(Schema $schema)
+    {
+        if ($this->hasTable) {
+            $this->addSql('RENAME TABLE filebase TO tmptable, tmptable TO FileBase');
+        }
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
