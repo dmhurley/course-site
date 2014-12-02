c5b33097494ac6e515ac4a72c456e14ccbcfb1c2
Nicholas Clawson <nickclaw@gmail.com>
2014-12-02 15:34:02 -0800

    Fix migration name.

--- app/DoctrineMigrations/Version20141125145243.php
+++ app/DoctrineMigrations/Version20141125145243.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20141120145124 extends AbstractMigration
+class Version20141125145243 extends AbstractMigration
 {
     private $hasTable;
 
