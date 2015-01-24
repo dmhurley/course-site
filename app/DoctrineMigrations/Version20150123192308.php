af0ce229d2a1efc2e03eba7219e8fd5a581545ec
Nicholas Clawson <nickclaw@gmail.com>
2015-01-23 20:16:02 -0800

    Fix a broken migration.

--- app/DoctrineMigrations/Version20150123192308.php
+++ app/DoctrineMigrations/Version20150123192308.php
@@ -14,16 +14,16 @@ class Version20150123192308 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE surveytaker DROP FOREIGN KEY FK_77785A44A3D10F50');
-        $this->addSql('ALTER TABLE surveytaker ADD CONSTRAINT FK_77785A44A3D10F50 FOREIGN KEY (studentID) REFERENCES AbstractUserStudent (id)');
+
+        $this->addSql('ALTER TABLE SurveyTaker DROP FOREIGN KEY FK_77785A44A3D10F50');
+        $this->addSql('ALTER TABLE SurveyTaker ADD CONSTRAINT FK_77785A44A3D10F50 FOREIGN KEY (studentID) REFERENCES AbstractUserStudent (id)');
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
+
         $this->addSql('ALTER TABLE SurveyTaker DROP FOREIGN KEY FK_77785A44A3D10F50');
         $this->addSql('ALTER TABLE SurveyTaker ADD CONSTRAINT FK_77785A44A3D10F50 FOREIGN KEY (studentID) REFERENCES abstractuserstudent (id) ON DELETE CASCADE');
     }
